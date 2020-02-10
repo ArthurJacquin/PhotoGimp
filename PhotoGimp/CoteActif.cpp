7b8b0b5e46373a104a5e1b1d79e2d3d6cdb0ce07
Attika <attika.mayou@gmail.com>
2020-02-10 23:04:54 +0100

    WIP CoteActif

--- PhotoGimp/CoteActif.cpp
+++ PhotoGimp/CoteActif.cpp
@@ -4,14 +4,24 @@ CoteActif::CoteActif()
 {
 	y_max = 0.0f;
 	curr_x = 0.0f;
-	coeff = 0.0f;
+	coeff_dir = 0.0f;
+	next_cote = nullptr;
 }
 
 CoteActif::CoteActif(float yMax, float currX, float coeff)
 {
 	y_max = yMax;
 	curr_x = currX;
-	coeff = coeff;
+	coeff_dir = coeff;
+	next_cote = nullptr;
+}
+
+CoteActif::CoteActif(float yMax, float currX, float coeff, CoteActif& nextCote)
+{
+	y_max = yMax;
+	curr_x = currX;
+	coeff_dir = coeff;
+	next_cote = &nextCote;
 }
 
 void CoteActif::SetCurrX(float const currX)
--- PhotoGimp/CoteActif.h
+++ PhotoGimp/CoteActif.h
@@ -7,13 +7,15 @@ private :
 public :
 	float y_max; //ordonnée maximale du côté
 	float curr_x; //abscisse de l'intersection
-	float coeff; //coefficient directeur
+	float coeff_dir; //coefficient directeur
+	CoteActif* next_cote; //côté actif suivant
 
 	CoteActif();
 	CoteActif(float yMax, float currX, float coeff);
+	CoteActif(float yMax, float currX, float coeff, CoteActif& nextCote);
 
 	void SetCurrX(float const currX);
 	float GetYMax() const { return y_max; };
 	float GetCurrX() const { return curr_x; };
-	float GetCoeff() const { return coeff; };
+	float GetCoeff() const { return coeff_dir; };
 };
