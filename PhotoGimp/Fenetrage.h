a261d017062287cf6f6dc511512fa9b0d0595e55
Margot <gotgot07@hotmail.fr>
2020-02-13 14:13:23 +0100

    Concave ça marche mais le découpage de la forme est en cours

--- PhotoGimp/Fenetrage.h
+++ PhotoGimp/Fenetrage.h
@@ -4,6 +4,7 @@
 
 Vertex intersection(Vertex FormePoint, Vertex FormePointNext, Vertex FenetrePoint, Vertex FenetreNextPoint);
 bool coupe(Vertex PointToTest, Vertex FormePointNext, Vertex FenetrePoint, Vertex FenetreNextPoint);
-bool visible(Vertex PointToTest, Vertex FenetrePoint, Vertex FenetreNextPoint);
+bool visible(Vertex PointToTest, Vertex FenetrePoint, Vertex FenetreNextPoint, bool cw);
 std::vector<Vertex> Fenetrage(std::vector<Vertex> PL_Forme, std::vector<Vertex> PW_Fenetre);
 bool CheckIfClockwise(std::vector<Vertex> v);
+std::vector<Vertex> isConcave(std::vector<Vertex> PW_Fenetre);
