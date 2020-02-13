50368077ebf7bd97b2cc1505f5e47092730640ee
ArthurJacquin <artuj38@free.fr>
2020-02-13 14:16:22 +0100

    Remplissage

--- PhotoGimp/Remplissage.h
+++ PhotoGimp/Remplissage.h
@@ -4,4 +4,4 @@
 std::vector<Vertex> RectEG(std::vector<Vertex> Poly); //retourne les coordonnées du rectangle englobant 
 std::vector<Vertex> GetCurrentLineFromRect(std::vector<Vertex> rect, double y); //retourne les coordonnées de la ligne du rectangle englobant à Y = y
 std::vector<Vertex> FindIntersectionWithLine(std::vector<Vertex> rect, std::vector<Vertex> Line, std::vector<Vertex> Poly); //retourne les intersections pour chaque ligne du rectangle englobant
-std::vector<Vertex> DebugRemplissage(std::vector<Vertex> Poly);
+std::vector<Vertex> Remplissage(std::vector<Vertex> Poly);
